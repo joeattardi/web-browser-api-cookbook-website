f179f3214af0c02ad783f8f1e8a1303a34bbbd61
Joe Attardi <jattardi@gmail.com>
2023-07-21 22:05:59 -0400

    Add Google Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,15 @@
 </script>
 
 <svelte:head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z5LMK9PJKM"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-Z5LMK9PJKM');
+  </script>
+
   <script>
     const darkQuery = window.matchMedia?.('(prefers-color-scheme: dark)');
     const cachedTheme = sessionStorage.getItem('theme');

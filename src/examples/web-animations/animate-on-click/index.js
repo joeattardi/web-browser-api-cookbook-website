6e50409a068245154dea7046831234f2a103df58
Joe Attardi <jattardi@gmail.com>
2023-08-17 07:34:21 -0400

    Add ripple effect demo

--- src/examples/web-animations/animate-on-click/index.js
+++ src/examples/web-animations/animate-on-click/index.js
@@ -1,12 +1,18 @@
 const button = document.querySelector('#button');
 
-button.addEventListener('click', () => {
-  button.animate([
-    { transform: 'scale(1)' },
-    { transform: 'scale(1.3) rotate(15deg)', offset: 0.5 },
-    { transform: 'scale(1)' }
+const ripple = button.querySelector('.ripple');
+ripple.style.width = `${button.offsetWidth / 2}px`;
+ripple.style.height = `${button.offsetWidth / 2}px`;
+
+button.addEventListener('click', event => {
+  const ripple = button.querySelector('.ripple');
+  ripple.style.top = `${event.offsetY - (ripple.offsetHeight / 2)}px`;
+  ripple.style.left = `${event.offsetX - (ripple.offsetWidth / 2)}px`;
+
+  ripple.animate([
+    { transform: 'scale(0)', opacity: 0.75 },
+    { transform: 'scale(4)', opacity: 0 }
   ], {
-    duration: 1000,
-    easing: 'ease-out'
+    duration: 600
   });
 });
--- src/examples/web-animations/animate-on-click/index.md
+++ src/examples/web-animations/animate-on-click/index.md
@@ -1,5 +1,5 @@
 ---
-title: Animating an Element on Click
+title: Applying a Ripple Effect on Click
 order: 1
 chapter: web-animations
 slug: web-animations/animate-on-click
@@ -9,4 +9,4 @@ summary: Trigger an animation when clicking on an element.
 Animate an element by calling its `animate` method, passing a set of keyframes and options. Keyframes can be specified as an array
 of objects containing CSS properties to animate.
 
-Each keyframe can also have an `offset` property to specify its position on the animation timeline. An `offset` of `0.25` is 25%, `0.5` is 50%, etc.
+You can use the click location to set the position of the ripple element, then animate its scale and opacity.

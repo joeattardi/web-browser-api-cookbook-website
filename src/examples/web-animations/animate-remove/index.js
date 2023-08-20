5f35f76b7ce607e73b2134b39d75291c7d779f59
Joe Attardi <jattardi@gmail.com>
2023-08-19 22:41:28 -0400

    Add new items to remove demo

--- src/examples/web-animations/animate-remove/index.js
+++ src/examples/web-animations/animate-remove/index.js
@@ -2,13 +2,34 @@ async function removeItem(item) {
   await item.animate([
     { opacity: 1, transform: 'translateX(0)', height: `${item.offsetHeight}px` },
     { opacity: 0, transform: 'translateX(-100%)', height: 0, padding: 0 }
-  ], { duration: 500, fill: 'both', easing: 'ease-in-out' }).finished;
+  ], { duration: 250, fill: 'both', easing: 'ease-in-out' }).finished;
 
   item.remove();
 }
+
+function addItem() {
+  const list = document.querySelector('.list-group');
+  const newItem = document.createElement('li');
+  newItem.textContent = 'A new item';
+  newItem.className = 'list-group-item';
+  newItem.addEventListener('click', () => {
+    removeItem(newItem);
+  });
+
+  list.insertBefore(newItem, list.firstElementChild);
+
+  newItem.animate([
+    { opacity: 0, transform: 'translateX(-100%)', height: 0, padding: 0 },
+    { opacity: 1, transform: 'translateX(0)', height: `${newItem.offsetHeight}px` }
+  ], { duration: 250, fill: 'both', easing: 'ease-in-out' })
+}
+
+document.querySelector('.add-button').addEventListener('click', () => {
+  addItem();
+});
 
 document.querySelectorAll('li').forEach(item => {
   item.addEventListener('click', () => {
     removeItem(item);
   });
-})
+});
--- src/examples/web-animations/animate-remove/index.md
+++ src/examples/web-animations/animate-remove/index.md
@@ -6,4 +6,4 @@ slug: web-animations/animate-remove
 summary: Remove an element from the page with an animation effect.
 ---
 
-Click on a list item to remove it with an animation effect.
+Click on a list item to remove it with an animation effect. You can also click Add Item to add a new item with an animation.

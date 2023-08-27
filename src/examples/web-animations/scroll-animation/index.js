ea2447b8148c86a4dab1ceea31bf16808a797944
Joe Attardi <jattardi@gmail.com>
2023-08-26 21:11:38 -0400

    Add comments to scroll linked animation

--- src/examples/web-animations/scroll-animation/index.js
+++ src/examples/web-animations/scroll-animation/index.js
@@ -1,11 +1,16 @@
 const progress = document.querySelector('.scroll-progress');
 const container = document.querySelector('.scroll-container');
 
+// Create a timeline that's linked to the document's
+// scroll position.
 const timeline = new ScrollTimeline({
   source: container
 });
 
-progress.animate([
-  { transform: 'scaleX(0)' },
-  { transform: 'scaleX(1)' }
-], { timeline, fill: 'both' });
+// Start the animation, passing the timeline you just created.
+progress.animate(
+  [
+    { transform: 'scaleX(0)' },
+    { transform: 'scaleX(1)' }
+  ],
+  { timeline });

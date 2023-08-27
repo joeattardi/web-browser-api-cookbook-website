67d435cf503c039176cad3b5e4b220a6679fe1a4
Joe Attardi <jattardi@gmail.com>
2023-08-26 21:11:27 -0400

    Add bounce example

---
+++ src/examples/web-animations/bounce/index.js
@@ -0,0 +1,30 @@
+const button = document.querySelector('#bounce-trigger');
+const ball = document.querySelector('#bounce');
+
+// The distance the element moves is smaller each time.
+const distances = [ '50%', '25%', '10%' ];
+
+async function animateBounce() {
+  for (let distance of distances) {
+    // Wait for this animation to complete before continuing
+    await ball.animate([
+      // Start at the bottom
+      { transform: 'translateY(0)' }, 
+
+      // Move up by the current distance
+      { transform: `translateY(-${distance})`, offset: 0.5 },
+
+      // Back to the bottom
+      { transform: 'translateY(0)' }
+    ], {
+      duration: 250,
+      easing: 'ease-in-out'
+    }).finished;
+  }
+}
+
+button.addEventListener('click', async () => {
+  button.disabled = true;
+  await animateBounce();
+  button.disabled = false;
+});
---
+++ src/examples/web-animations/bounce/index.md
@@ -0,0 +1,9 @@
+---
+title: Making an Element Bounce
+order: 6
+chapter: web-animations
+slug: web-animations/bounce
+summary: Use multiple animations in a sequence to create a bounce effect.
+---
+
+This replicates a bouncing icon effect such as the one in macOS by moving the element up and down again. The animation is repeated three times, each with a smaller transformation value, so that it appears to bounce.

40345d6cad3d7bca85d94700f40f060ee7c0849d
Joe Attardi <jattardi@gmail.com>
2023-09-21 21:49:22 -0400

    Add some comments

--- src/examples/files/drag-drop-file/index.js
+++ src/examples/files/drag-drop-file/index.js
@@ -1,12 +1,22 @@
 const target = document.querySelector('.drop-target');
 
 target.addEventListener('drop', event => {
+  // Cancel the drop event. Otherwise, the browser will leave the page
+  // and navigate to the file directly.
   event.preventDefault();
+
+  // Get the selected file data.
   const [item] = event.dataTransfer.items;
   const file = item.getAsFile();
 
-  const reader = new FileReader();
+  // Do nothing if the dropped file is not an image.
+  if (!file.type.startsWith('image/')) {
+    return;
+  }
 
+  // Read the file data and insert the loaded image
+  // into the page.
+  const reader = new FileReader();
   reader.addEventListener('load', event => {
     const image = document.createElement('img');
     image.src = event.target.result;
@@ -17,6 +27,8 @@ target.addEventListener('drop', event => {
   reader.readAsDataURL(file);
 });
 
+// You need to cancel the dragover event as well, to prevent the
+// file from replacing the full page content
 target.addEventListener('dragover', event => {
   event.preventDefault();
 });
--- src/examples/files/loading-video/index.js
+++ src/examples/files/loading-video/index.js
@@ -4,7 +4,10 @@ const content = document.querySelector('#file-content');
 fileInput.addEventListener('change', event => {
   const [file] = fileInput.files;
 
-  // File extends from Blob
+  // File extends from Blob, which can be passed to
+  // createObjectURL.
   const objectUrl = URL.createObjectURL(file);
+
+  // The <video> element can take the object URL to load the video
   content.src = objectUrl;
 });
